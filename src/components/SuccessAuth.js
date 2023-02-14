6bda1e5dd005079da65d4296e62fa44d41437121
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-02-14 14:26:29 +0000

    fixed import

--- src/components/SuccessAuth.js
+++ src/components/SuccessAuth.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
-import {supabase} from "../supabase/supabaseClient.js";
+import supabase from "../supabase/supabaseClient.js";
 
 const SuccessAuth = () => {
     const [user, setUser] = useState({});
