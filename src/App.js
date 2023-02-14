6cab28637cd3c7e5ba1b3224f461309b1724fa90
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-02-14 14:25:07 +0000

    added routes for login & register components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import NavBar from "./components/Navbar";
-import AuthUser from './components/AuthUser';
+import Login from './components/Login.jsx';
 import SuccessAuth from './components/SuccessAuth';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
 
     return (
         <div className="App">
             <NavBar/>
-            <div className="App-header">
-
                 <Router>
                     <Routes>
-                        <Route path={'/login'} element={<AuthUser/>}/>
+                        <Route path={'/login'} element={<Login/>}/>
+                        <Route path={'/register'} element={<Register/>}/>
+                        <Route path={'/'} />
                         <Route path={'/auth'} element={<SuccessAuth/>}/>
                     </Routes>
                 </Router>
-            </div>
 
 
         </div>
