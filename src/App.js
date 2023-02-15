199cc922659f8d9526271eb3b02fffafa600cb05
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-02-15 14:09:21 +0000

    added new component to correct react route

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import Login from './components/Login.jsx';
 import SuccessAuth from './components/SuccessAuth';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Register from "./components/Register";
+import CreateWorkout from "./components/CreateWorkout";
 
 function App() {
 
     return (
-        <div className="App">
+        <div className="App min-h-full font-Poppins box-border">
             <NavBar/>
-                <Router>
-                    <Routes>
-                        <Route path={'/login'} element={<Login/>}/>
-                        <Route path={'/register'} element={<Register/>}/>
-                        <Route path={'/'} />
-                        <Route path={'/auth'} element={<SuccessAuth/>}/>
-                    </Routes>
-                </Router>
+            <Router>
+                <Routes>
+                    <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/register'} element={<Register/>}/>
+                    <Route path={'/'}/>
+                    <Route path={'/auth'} element={<SuccessAuth/>}/>
+                    <Route path={'/create'} element={<CreateWorkout/>}/>
+                </Routes>
+            </Router>
 
 
         </div>
