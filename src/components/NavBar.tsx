0da8ec3a679020d2d8fdd8218ac9b7e65e01c974
DomDevs2000 <aidan@carvalho.org.uk>
2023-03-30 16:48:20 +0100

    fixed bug where users werent logged out immediately when pressed loggout | added delete localstorage to logout function

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,79 +1,86 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import supabase from "../lib/supabaseClient";
 
-import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const [user, setUser] = useState({});
-  // const navigate = useNavigate()
+    const [user, setUser] = useState({});
 
-  supabase.auth.getUser();
+    supabase.auth.getUser();
 
-  useEffect(() => {
-    return () => {
-      async function getUserData() {
-        await supabase.auth.getUser().then((value) => {
-          if (value.data?.user) {
-            setUser(value.data.user);
-            // console.log(value.data.user);
-          }
-        });
-      }
-      getUserData();
-    };
-  }, []);
+    useEffect(() => {
+        return () => {
+            async function getUserData() {
+                await supabase.auth.getUser().then((value) => {
+                    if (value.data?.user) {
+                        setUser(value.data.user);
+                        console.log(value.data.user);
+                    }
+                });
+            }
 
-  async function logOutUser() {
-    const { error } = await supabase.auth.signOut();
-    console.log("user logged out");
-    // navigate('/login');
-  }
+            getUserData();
+        };
+    }, []);
 
-  return (
-    <>
-      <header className="text-white bg-red-600">
-        <nav className=" py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
-          <div className="flex items-center gap-x-4">
-            <img
-              className="w-14"
-              src={require("../assets/images/dumbbell-light.png")}
-              alt=""
-            />
-            <a href="/" className="cursor-pointer">
-              <button>
-                <h1 className="text-lg">Workout Tracker</h1>
-              </button>
-            </a>
-          </div>
-          <ul className="flex flex-1 justify-end gap-x-10">
-            <li>
-              <a href="/create" className="cursor-pointer">
-                <button>Track Workout</button>
-              </a>
-            </li>
+    async function logOutUser() {
+        const {error} = await supabase.auth.signOut();
+        localStorage.removeItem('sb-snturtquxvvxlltvqekf-auth-token')
+        console.log('User Logged Out')
+        console.log("user logged out");
+    }
 
-            {Object.keys(user).length > 0 ? (
-              <li className="cursor-pointer">
-                {" "}
-                <a
-                  href="/login"
-                  onClick={() => {
-                    logOutUser();
-                  }}
-                >
-                  Logout
-                </a>
-              </li>
-            ) : (
-              <a href="/login" className="cursor-pointer">
-                <button>Login</button>
-              </a>
-            )}
-          </ul>
-        </nav>
-      </header>
-    </>
-  );
+    useEffect(() => {
+        async function logOut() {
+
+        }
+
+        logOut()
+    }, [user]);
+    return (
+        <>
+            <header className="text-white bg-red-600">
+                <nav className=" py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+                    <div className="flex items-center gap-x-4">
+                        <img
+                            className="w-14"
+                            src={require("../assets/images/dumbbell-light.png")}
+                            alt=""
+                        />
+                        <a href="/" className="cursor-pointer">
+                            <button>
+                                <h1 className="text-lg">Workout Tracker</h1>
+                            </button>
+                        </a>
+                    </div>
+                    <ul className="flex flex-1 justify-end gap-x-10">
+                        <li>
+                            <a href="/create" className="cursor-pointer">
+                                <button>Track Workout</button>
+                            </a>
+                        </li>
+
+                        {Object.keys(user).length > 0 ? (
+                            <li className="cursor-pointer">
+                                {" "}
+                                <a
+                                    href="/login"
+                                    onClick={() => {
+                                        logOutUser();
+                                    }}
+                                >
+                                    Logout
+                                </a>
+                            </li>
+                        ) : (
+                            <a href="/login" className="cursor-pointer">
+                                <button>Login</button>
+                            </a>
+                        )}
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
 };
 
 export default Navbar;
