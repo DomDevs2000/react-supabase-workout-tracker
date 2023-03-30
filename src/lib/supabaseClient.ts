bb2eade7208ec8280eff05f0932f60eaafd15d27
DomDevs2000 <aidan@carvalho.org.uk>
2023-03-30 16:14:28 +0100

    removed unused type

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,19 +1,15 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "../../types/supabase";
+import {createClient} from "@supabase/supabase-js";
+import {Database} from "../../types/supabase";
 
 // const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 // const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
-type supabase = {
-  supabaseUrl: string;
-  supabaseAnonKey: string;
-};
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 const supabase = createClient<Database>(
-  supabaseUrl as string,
-  supabaseAnonKey as string
+    supabaseUrl as string,
+    supabaseAnonKey as string
 );
 export default supabase;
