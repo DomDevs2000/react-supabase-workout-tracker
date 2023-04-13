add9689ecced76685b5830af12d6ea970a82d067
DomDevs2000 <aidan@carvalho.org.uk>
2023-04-13 15:32:58 +0100

    added metrics to aws s3 bucket via terraform

--- main.tf
+++ main.tf
@@ -58,3 +58,8 @@ resource "aws_s3_bucket_website_configuration" "bucket-config" {
     }
   }
 }
+
+resource "aws_s3_bucket_metric" "example-entire-bucket" {
+  bucket = aws_s3_bucket.workout-tracker.id
+  name   = "EntireBucket"
+}
